dd7aa23d1a8246657f2c332610bab5dafbe2beca
Heather Cook <2375346c@student.gla.ac.uk>
2020-10-24 14:26:11 +0100

    added line

--- practice.py
+++ practice.py
@@ -20,44 +20,32 @@ mixer.music.set_volume(0.7)
 # Start playing the song
 mixer.music.play()
 
-
-
 white_ghost = pygame.image.load('ghosts/white ghost.png').convert_alpha()
-white_ghost = pygame.transform.scale(white_ghost,(61,82))
+white_ghost = pygame.transform.scale(white_ghost, (61, 82))
 
 green_ghost = pygame.image.load('ghosts/green ghost.png').convert_alpha()
-green_ghost = pygame.transform.scale(green_ghost,(61,82))
+green_ghost = pygame.transform.scale(green_ghost, (61, 82))
 
 orange_ghost = pygame.image.load('ghosts/orange ghost.png').convert_alpha()
-orange_ghost = pygame.transform.scale(orange_ghost,(61,82))
+orange_ghost = pygame.transform.scale(orange_ghost, (61, 82))
 
 purple_ghost = pygame.image.load('ghosts/purple ghost.png').convert_alpha()
-purple_ghost = pygame.transform.scale(purple_ghost,(61,82))
-
+purple_ghost = pygame.transform.scale(purple_ghost, (61, 82))
 
 ghost_pos = 0
 
 
-#pos = 0:"white", 1:"green", 2:"orange", 3:"purple"
+# pos = 0:"white", 1:"green", 2:"orange", 3:"purple"
 def ghost(pos):
     if pos == 0:
-        screen.blit(white_ghost,(132,503))
+        screen.blit(white_ghost, (132, 503))
     if pos == 1:
-        screen.blit(green_ghost,(427,503))
+        screen.blit(green_ghost, (427, 503))
     if pos == 2:
-        screen.blit(orange_ghost,(780,503))
+        screen.blit(orange_ghost, (780, 503))
     if pos == 3:
-        screen.blit(purple_ghost,(1107,503))
+        screen.blit(purple_ghost, (1107, 503))
 
-def position(pos):
-    if pos == 0:
-        return (132,503)
-    if pos == 1:
-        return (427,503)
-    if pos == 2:
-        return (780,503)
-    if pos == 3:
-        return (1107,503)
 
 game_running = True
 while game_running:
@@ -72,30 +60,31 @@ while game_running:
                 sys.exit()
 
             if event.key == pygame.K_a:
-                #sets ghost_position
+                # sets ghost_position
                 ghost_pos = 0;
 
             if event.key == pygame.K_s:
-                #sets ghost_position
+                # sets ghost_position
                 ghost_pos = 1;
-                
-            if event.key == pygame.K_d:
-                #sets ghost_position
-                ghost_pos = 2;
-                
-            if event.key == pygame.K_f:
-                #sets ghost_position
-                ghost_pos = 3;
 
-                
+            if event.key == pygame.K_d:
+                # sets ghost_position
+                ghost_pos = 2;
+
+            if event.key == pygame.K_f:
+                # sets ghost_position
+                ghost_pos = 3;
 
             if event.key == pygame.K_p:
                 mixer.music.pause()
             if event.key == pygame.K_e:
                 mixer.music.unpause()
 
-
     screen.fill(colour)
+    pygame.draw.rect(screen, (255, 255, 255), [(0, 470), (350, 10)])
+    pygame.draw.rect(screen, (82, 172, 0), [(350, 470), (350, 10)])
+    pygame.draw.rect(screen, (242, 143, 28), [(650, 470), (350, 10)])
+    pygame.draw.rect(screen, (110, 58, 158), [(950, 470), (350, 10)])
     ghost(ghost_pos)
 
     clock.tick(60)
