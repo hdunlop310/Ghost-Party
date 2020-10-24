c50d80abf7c2e993103f9bc6d8f8f81afaf6234a
Heather Cook <2375346c@student.gla.ac.uk>
2020-10-24 12:31:51 +0100

    added ghost movement kinda

--- practice.py
+++ practice.py
@@ -2,13 +2,20 @@ import pygame, sys, random
 
 pygame.init()
 
-screen = pygame.display.set_mode([1500, 1000])
+screen = pygame.display.set_mode([1300, 600])
 
-width = 1500
-height = 1000
+width = 1300
+height = 600
 colour = (49,49,49)
-
 clock = pygame.time.Clock()
+
+ghost_image = pygame.image.load('ghost.png')
+ghostx = 30
+ghosty = 30
+
+def ghost(x, y):
+    screen.blit(ghost_image, (x, y))
+
 game_running = True
 while game_running:
     for event in pygame.event.get():
@@ -22,16 +29,19 @@ while game_running:
                 sys.exit()
             elif event.key == pygame.K_a:
                 print("player pressed a")
-                pygame.draw.rect(screen, (255, 255, 255), [(50, 50), (30, 30)])
+                #change variable holding dimentions/colour
+                ghostx += 10
                 pygame.display.update()
             elif event.key == pygame.K_d:
                 print("player pressed d")
-                pygame.draw.rect(screen, (0, 255, 255), [(50, 50), (30, 30)])
+                #change variable holding dimentions/colour
                 pygame.display.update()
+        ghost(ghostx, ghosty)
+        pygame.display.update()
 
     screen.fill((colour))
 
-    pygame.draw.rect(screen, (0, 0, 255), [(30, 30), (30, 30)])
+    #pygame.draw.rect(screen, (0, 0, 255), [(30, 30), (30, 30)])
     pygame.display.flip()
     clock.tick(60)
     pygame.display.update()
