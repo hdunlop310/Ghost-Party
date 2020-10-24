9ee804fd0ab11fb8db140131939069804a0faa58
Heather Cook <2375346c@student.gla.ac.uk>
2020-10-24 13:18:16 +0100

    pause and unpause

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 import pygame, sys, random
+from pygame import mixer
 
 pygame.init()
 
@@ -6,13 +7,18 @@ screen = pygame.display.set_mode([1300, 600])
 
 width = 1300
 height = 600
-colour = (49,49,49)
+colour = (49, 49, 49)
 clock = pygame.time.Clock()
-music = 'sounds/mastered maybe (Mastered with Thunder at 50pct).wav'
-pygame.mixer.init()
-pygame.mixer.music.load(music)
-pygame.mixer.music.play()
-pygame.event.wait()
+mixer.init()
+
+# Loading the song
+mixer.music.load("sounds/mastered maybe (Mastered with Thunder at 50pct).wav")
+
+# Setting the volume
+mixer.music.set_volume(0.7)
+
+# Start playing the song
+mixer.music.play()
 player_white = pygame.image.load('ghosts/white ghost.png')
 
 white_ghost = pygame.image.load('ghosts/white ghost.png')
@@ -23,16 +29,18 @@ purple_ghost = pygame.image.load('ghosts/purple ghost.png')
 ghostx = 30
 ghosty = 30
 
-#x = x-coords, y = y-coords, which_ghost = "white", "green", "orange", "purple"
+
+# x = x-coords, y = y-coords, which_ghost = "white", "green", "orange", "purple"
 def ghost(x, y, which_ghost):
-    if(which_ghost == "white"):
+    if which_ghost == "white":
         screen.blit(white_ghost, (x, y))
-    if(which_ghost == "green"):
+    if which_ghost == "green":
         screen.blit(green_ghost, (x, y))
-    if(which_ghost == "orange"):
+    if which_ghost == "orange":
         screen.blit(orange_ghost, (x, y))
-    if(which_ghost == "purple"):
+    if which_ghost == "purple":
         screen.blit(purple_ghost, (x, y))
+
 
 game_running = True
 while game_running:
@@ -45,16 +53,22 @@ while game_running:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-                
+
             if event.key == pygame.K_a:
                 print("player pressed a")
-                #change variable holding dimentions/colour
+                # change variable holding dimentions/colour
                 ghostx -= 10
-                
+
             if event.key == pygame.K_d:
                 ghostx += 10
                 print("player pressed d")
-                #change variable holding dimentions/colour
+                # change variable holding dimentions/colour
+
+            if event.key == pygame.K_p:
+                mixer.music.pause()
+            if event.key == pygame.K_e:
+                mixer.music.unpause()
+
 
     screen.fill(colour)
     ghost(ghostx, ghosty, "orange")
